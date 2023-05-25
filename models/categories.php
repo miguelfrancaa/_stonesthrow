74dd1b229824fc71d43dc4f5866a5c22b7cc3eb5
miguelfrancaa <miguelcrfranca@gmail.com>
2023-05-25 18:23:35 +0100

    Model de Categories criado para poder aplicar na loja.

---
+++ models/categories.php
@@ -0,0 +1,18 @@
+<?php
+	require_once("base.php");
+
+	class Categories extends Base{
+
+		public function getCategories1(){
+			$query = $this->db->prepare("
+			SELECT *
+			FROM categories
+			WHERE parent_id IS NULL
+			");
+
+			$query->execute([]);
+
+			return = $query->fetchAll();
+		}
+	}
+?>
---
+++ views/shop.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>STONES THROW</title>
+		<meta name="keywords" content="STONES THROW">
+		<link rel="icon" href="img/logost.ico">
+		<meta name="description" content="stones throw, records, hiphop, music, alternative, quasimoto, mfdoom, underground, label">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+<?php
+	require("includes/header.php");
+?>
+	<main>
+		<div class="container">
+			<div class="row shopCat">
+<?php
+	foreach($categories as $category) {
+		echo "<div class='col-md-6 col-sm-12 col-xs-12 shopCat2'>
+					<a href='subcategories.php?subcategory_id=".$category["category_id"]."'><h2 class='name'>".$category["name"]."</h2></a>
+					</div>";
+	}
+?>
+			</div>
+		</div>
+	</main>
+
+	<?php
+	require("includes/footer.php");
+	?>
+
+
+
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+	</body>
+</html>
